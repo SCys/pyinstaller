412602624807b90e37803abcaea887596567b190
SCys <me@iscys.com>
2018-06-17 15:50:49 +0800

    * update script

--- use_chinese_cdn.sh
+++ use_chinese_cdn.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-curl -s iscys.com | grep -q -i china && sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories
+
+# Optmize chinese mirror
+IN_CHINA=`curl -s iscys.com | grep -q -i china && echo true || echo false`
+if [ "$IN_CHINA" == "true" ]; then 
+    sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories
+    echo "Use china mirror:mirrors.tuna.tsinghua.edu.cn"
+fi
